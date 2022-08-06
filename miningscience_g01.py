7f5d8513503c83bac419a215d8a3c292ad31fec3
josand00 <105082571+josand00@users.noreply.github.com>
2022-08-06 12:07:45 -0500

    Josue Choez realizo cambios en la pregunta 0

---
+++ miningscience_g01.py
@@ -0,0 +1,61 @@
+# NOMBRE (apellido, nombre):
+
+# Carga de librerías necesarias
+
+
+def download_pubmed(     ): 
+    """
+    Docstring download_pubmed
+    
+    
+    
+    
+    
+    """
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    return 
+
+
+
+def mapscience(       ):
+    """
+    Docstring map_science
+    
+    
+    
+    
+    
+    
+    
+    """
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    return 
+
+    
